a617cd4a623c3a60b95750253aa0e089a3d7816e
rjimenezg73 <robertojimenezg@gmail.com>
2023-04-29 00:44:02 -0600

    Intro JS Moderno hasta video 5(Template String)

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y constantes
+// No usar var, investigar el por que en la documentación de Mozilla
+
+const nombre = "Roberto";
+const apellido = "Jiménez";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+  let valorDado = 6;
+  console.log(valorDado);
+}
+
+console.log(valorDado);
