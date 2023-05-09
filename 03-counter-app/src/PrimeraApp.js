c34520c5b0f1cc176b907a7a228a4213f7220ffe
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-08 21:02:48 -0600

    Sec 4 - Videos 1 al 9

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Functional Component
+
+const PrimeraApp = () => {
+
+  return (
+    <>
+      <h1>Hola Mundo React!</h1>
+      <p>Mi primera aplicación</p>
+    </>
+  );
+
+    
+
+  
+};
+
+
+export default PrimeraApp;
