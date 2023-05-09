4270e5dfe7c418a7140def23cac11e67829205d9
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-08 23:12:42 -0600

    Sec 4 - Vid 12 y 13

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// Es casi necesario que ses PropTypes
 
 // Functional Component
 
-const PrimeraApp = (props) => {
+const PrimeraApp = ({saludo, subtitulo }) => {
 
   return (
     <>
-      <h1>{props.saludo}</h1>
-      <p>Mi primera aplicación</p>
+      <h1>{saludo}</h1>
+      <p>{subtitulo}</p>
     </>
   );
 
-    
-
-  
 };
 
+// Y aquí debe de ser .propTypes y en la propiedad PropTypes
+PrimeraApp.propTypes = {
+  //saludo: PropTypes.string   Has aquí requiere que la propiedad sea string pero no obligatoria
+  saludo: PropTypes.string.isRequired   // Ahora si es obligatirio
+}
+
+PrimeraApp.defaultProps = {
+  subtitulo: 'Soy un subtitulo'
+}
 
 export default PrimeraApp;
