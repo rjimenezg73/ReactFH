6b39fa35436e2e429e5bf4e3e4b9b2a97003a361
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-08 23:30:31 -0600

    Sec 4 - Vid 14 - Tarea CounterApp

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+
+  console.log(value);
+
+  return(
+    <>
+      <h1>CounterApp</h1>
+      <h2>{value}</h2>
+    </>
+  )
+ 
+}
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 
 const PrimeraApp = ({saludo, subtitulo }) => {
 
+
   return (
     <>
       <h1>{saludo}</h1>
